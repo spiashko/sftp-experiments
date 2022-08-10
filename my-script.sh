e8129dfa6ee4d5a0dffedd00c6dfb3b82a47845a
Siarhei Piashko <peshko.sergey@gmail.com>
2022-08-10 21:28:05 +0200

    fix

--- my-script.sh
+++ my-script.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-SSHPASS=test sshpass -e sftp -o StrictHostKeyChecking=no -o Port=2222 admin@localhost:/home/admin/export <<EOF
+SSHPASS=test sshpass -e sftp -o StrictHostKeyChecking=no -o Port=2222 admin@localhost:/export <<EOF
 ls
 exit
 EOF
